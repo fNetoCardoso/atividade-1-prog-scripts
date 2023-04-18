ff47f61b281216eb52d46c309738fde6a420d7e2
fNetoCardoso <francisco14042000@gmail.com>
2023-04-18 07:56:17 -0300

    inicio

---
+++ src/components/LoadingPage/index.tsx
@@ -0,0 +1,33 @@
+import { Contexto } from "../../contexts";
+import { useContext, useEffect } from "react";
+import "./LoadingPage.css";
+import { GetJogo} from "../../services/JogoService";
+
+function LoadingPage() {
+    const {loaded,setLotoFacil, setMegaSena, setLoaded} = useContext(Contexto)
+    useEffect(()=>{
+        setTimeout(()=>{
+            GetJogo().then((r)=>{
+                setLotoFacil(r.lotofacil)
+                setMegaSena(r.megaSena)
+                setLoaded(true)
+            })
+
+        }, 3000)
+
+    },[setLoaded, setLotoFacil, setMegaSena])
+    return (
+        <>
+
+            <div className="loadingPage" style={{ display: loaded ? "none" : "" }}>
+                <div className="loading">
+                    <h1>
+                        Carregando Resultados...
+                    </h1>
+                </div>
+            </div>
+          <hr style={{ display: loaded ? "" : "none" }} />
+        </>
+    )
+}
+export default LoadingPage;
